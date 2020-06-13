5c8e28252fe2af05c94b6509b4c416bf12e65613
SanketJ <sanketj1112@gmail.com>
2020-06-13 00:05:00 -0400

    sanket jadhav

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
+// // const express = require("express");
+// const express = require("express");
+// const app = express();
 
-const app = express();
+// app.get("/", (req, res) => {
+//   res.send("How are you doing");
+// });
 
-app.get("/", (req, res) => {
-  res.send("How are you doing");
-});
+// app.listen(8080, () => {
+//   console.log("Listening on port 8080");
+// });
 
-app.listen(8080, () => {
-  console.log("Listening on port 8080");
-});
+exports.handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
